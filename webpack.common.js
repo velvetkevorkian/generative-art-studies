b71d607004c678436c3d6ee05fb01d8fbaefb754
Kyle <kyle@kylemacquarrie.co.uk>
2018-12-02 23:49:36 +0000

    Rename package.

--- webpack.common.js
+++ webpack.common.js
@@ -7,13 +7,13 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Generative Art Book Studies',
+      title: 'Generative Art Studies',
       template: 'index.html'
     })
   ],
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'generative-art-book-studies')
+    path: path.resolve(__dirname, 'generative-art-studies')
   },
   optimization: {
     splitChunks: {
